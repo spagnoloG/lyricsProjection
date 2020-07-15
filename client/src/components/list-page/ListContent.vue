1b93fed501929422ab2926b24535deeda7813528
Gasper <gasper.spagnolo@outlook.com>
2020-07-15 12:37:01 +0200

    finishing touches

--- client/src/components/list-page/ListContent.vue
+++ client/src/components/list-page/ListContent.vue
@@ -1,27 +1,60 @@
 <template>
   <b-container class="bv-example-row">
+    <div>
+      <b-alert
+        :show="dismissCountDown"
+        dismissible
+        variant="success"
+        @dismissed="dismissCountDown=0"
+        @dismiss-count-down="countDownChanged"
+      >Pesem uspešno izbrisana!</b-alert>
+    </div>
     <b-row class="text-center">
       <b-col></b-col>
       <b-col cols="10">
         <div v-for="lyric in lyrics" :key="lyric.index">
           <b-list-group>
-            <b-list-group-item  v-b-modal.modal-center
+            <b-list-group-item
+              v-b-modal.modal-center
               class="lyric-item"
               button
-              @click="testClick(lyric.index)"
-            >{{ lyric.title }}</b-list-group-item>
+              @click="selectLyric(lyric)"
+            >Pesem št: {{ lyric.index }}, naslov: {{ lyric.title }}</b-list-group-item>
           </b-list-group>
         </div>
       </b-col>
       <b-col></b-col>
     </b-row>
     <div>
-      <b-modal id="modal-center" centered title="Možnosti:" ok-only ok-variant="dark">
+      <b-modal ref="option-modal" id="modal-center" centered title="Možnosti:" ok-only ok-variant="dark">
         <h3>{{ selectedLyric.title }}</h3>
         <p>Številka pesmi: {{ selectedLyric.index }}</p>
-        <b-button :to="{ name: 'edit', params: { id: selectedLyric.index } }" variant="primary" pill class="update-delete-project-btn">Uredi</b-button>
-        <b-button :to="{ name: 'project', params: { id: selectedLyric.index }}" variant="warning" pill class="update-delete-project-btn">Projeciraj</b-button>
-        <b-button @click="deleteLyric()" variant="danger" pill class="update-delete-project-btn">Izbriši</b-button>
+        <b-button
+          :to="{ name: 'edit', params: { id: selectedLyric.index } }"
+          variant="primary"
+          pill
+          class="update-delete-project-btn"
+        >Uredi</b-button>
+        <b-button
+          :to="{ name: 'project', params: { id: selectedLyric.index }}"
+          variant="warning"
+          pill
+          class="update-delete-project-btn"
+        >Projeciraj</b-button>
+        <div>
+          <b-button
+            variant="danger"
+            pill
+            class="update-delete-project-btn m-1"
+            v-b-toggle.collapse-1
+          >Izbriši</b-button>
+          <b-collapse id="collapse-1">
+            <b-card>
+              Si prepričan/a?
+              <b-button class="final-delete-btn" @click="deleteLyric()" variant="outline-danger">Da</b-button>
+            </b-card>
+          </b-collapse>
+        </div>
       </b-modal>
     </div>
   </b-container>
@@ -35,7 +68,9 @@ export default {
     return {
       lyrics: [],
       selected: Number,
-      selectedLyric: Object
+      selectedLyric: Object,
+      dismissSecs: 5,
+      dismissCountDown: 0
     };
   },
   beforeCreate() {
@@ -57,10 +92,47 @@ export default {
       .catch(error => console.log(error));
   },
   methods: {
-    testClick(index) {
-      this.selectedLyric = this.lyrics[index -1];
+    selectLyric(lyric) {
+      this.selectedLyric = lyric;
     },
-    deleteLyric(){console.log("delete!")},
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs;
+    },
+    deleteLyric() {
+      axios
+        .delete("http://localhost:9000/lyrics/" + this.selectedLyric.index)
+        .then(res => {
+          console.log(res)
+          this.$refs['option-modal'].hide()
+          this.updateList()
+          this.showAlert();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    updateList() {
+      axios
+      .get("http://localhost:9000/lyrics")
+      .then(res => {
+        console.log(res);
+        this.lyrics.length = 0
+        const data = res.data;
+        for (let key in data) {
+          const lyric = {
+            index: data[key].index,
+            title: data[key].title,
+            content: data[key].content
+          };
+          this.lyrics.push(lyric);
+        }
+        console.log(this.lyrics);
+      })
+      .catch(error => console.log(error));
+    }
   }
 };
 </script>
@@ -70,6 +142,12 @@ export default {
   margin-bottom: 1%;
 }
 .update-delete-project-btn {
-  margin: auto 1% auto 1%;
+  margin: auto 1% 1% 1%;
+}
+
+.final-delete-btn {
+  position: absolute;
+  right: 20px;
+  bottom: 15px;
 }
 </style>
