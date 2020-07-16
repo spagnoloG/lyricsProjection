705a6763178d6fcc0d7d89837e77224d12953061
Gasper <gasper.spagnolo@outlook.com>
2020-07-16 11:04:57 +0200

    Fixed navbar

--- client/src/components/main-page/WelcomeTop.vue
+++ client/src/components/main-page/WelcomeTop.vue
@@ -13,10 +13,11 @@
         <strong class="glavni-napis">Župnija Šempeter pri Gorici</strong>
       </b-navbar-brand>
 
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <!-- Routes and Options -->
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto" right>
+        <b-navbar-nav class="ml-auto">
           <b-nav-item :to="{ path: 'project/0' }">Projeciraj</b-nav-item>
           <b-nav-item :to="{ name: 'list' }">Pregled</b-nav-item>
         </b-navbar-nav>
