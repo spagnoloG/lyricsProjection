6bc287be2a20f188144032d3bd61f822c0098a99
Gasper <gasper.spagnolo@outlook.com>
2020-07-24 10:20:26 +0200

    fixing docker files

--- server/api/server.js
+++ server/api/server.js
@@ -40,7 +40,7 @@ app.use('/api', require('./routes/index'));
 app.use('/lyrics', require('./routes/lyrics'));
 
 // Connect to database
-mongoose.connect("mongodb://192.168.122.117:27017/projectionDb",
+mongoose.connect("mongodb://localhost:27017/projectionDb",
     { useNewUrlParser: true, useUnifiedTopology: true}, () =>
     console.log('Connected to database')
 );
