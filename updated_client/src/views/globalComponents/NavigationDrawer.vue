16020eb8ea645c586369b94407ca8725e9434af3
Gasper Spagnolo <gasper.spagnolo@outlook.com>
2020-07-31 19:22:07 +0200

    Added a few routes and icons to navigation drawer.

--- updated_client/src/views/globalComponents/NavigationDrawer.vue
+++ updated_client/src/views/globalComponents/NavigationDrawer.vue
@@ -1,12 +1,40 @@
 <template>
     <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
+
         <v-list-item :to="{name: 'Home'}">
           <v-list-item-action>
             <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title>Domov</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item :to="{name: 'Playlists'}">
+          <v-list-item-action >
+            <v-icon>mdi-presentation-play</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Seznam predvajanj</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item :to="{name: 'List'}">
+          <v-list-item-action >
+            <v-icon>mdi-playlist-music</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Seznam pesmi</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item :to="{name: 'ListPsalms'}">
+          <v-list-item-action >
+            <v-icon>mdi-format-list-text</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Seznam psalmov</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
@@ -19,12 +47,12 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item :to="{name: 'List'}">
+        <v-list-item :to="{name: 'AddPsalm'}">
           <v-list-item-action >
-            <v-icon>mdi-playlist-music</v-icon>
+            <v-icon>mdi-script-text</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Seznam pesmi</v-list-item-title>
+            <v-list-item-title>Dodaj nov Psalm</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
