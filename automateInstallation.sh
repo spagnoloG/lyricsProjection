81ef09c9847c08724123a8a268fc8b5cbbbec05a
Gasper <gasper@pop-os.localdomain>
2020-09-09 16:19:20 +0200

    Fixed automate script.

---
+++ automateInstallation.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Requirements: local mongodb, yarn and global serve package
+echo "Welcome to lyrics projection app!"
+cd client
+echo "Building client"
+yarn install
+yarn build
+cd ..
+cd server/socketIO
+echo "Starting socket-io server"
+yarn install
+yarn serve &
+cd ..
+cd api
+echo "Starting api server.."
+yarn install
+yarn serve &
+cd ..
+cd ..
+echo "serving client"
+cd client 
+serve -s dist &
