0f02a7c897b6f4c0838d3d284a4397720a9d901f
Gasper <gasper@pop-os.localdomain>
2020-09-09 17:41:08 +0200

    Added on boot script for raspberry pi.

--- onBoot.sh
+++ onBoot.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 # Requirements: local mongodb, yarn and global serve package
-echo "Welcome to lyrics projection app"
-cd client
-echo "Building client..."
-yarn install
-yarn build
-cd ..
+echo "Starting lyrics projection app:)"
 cd server/socketIO
 echo "Starting socket-io server..."
-yarn install
 yarn serve &
 cd ..
 cd api
 echo "Starting api server..."
-yarn install
 yarn serve &
 cd ..
 cd ..
 echo "serving client..."
 cd client 
-serve -s dist &
-echo "Successfully completed installation and started app:)"
+serve -s dist & 
+echo "Successfully started projection app:)"
