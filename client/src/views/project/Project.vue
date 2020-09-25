b0697f4fa690cbb56fee1e33203b222a0ed13d1f
Gasper <gasper@pop-os.localdomain>
2020-09-25 10:46:53 +0200

    Removed unnecessary code in project component.

--- client/src/views/project/Project.vue
+++ client/src/views/project/Project.vue
@@ -1,13 +1,21 @@
 <template>
+<div>
+  <!-- Tab menu -->
+  <v-toolbar>
+    <v-btn
+      icon
+      :to="{ name: 'ListLyrics'}">
+      <v-icon>mdi-arrow-left-thick</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn
+      text
+      :to="{ name: 'EditLyric', params: { id: id }}">
+      Uredi besedilo pesmi
+    </v-btn>
+  </v-toolbar>
+  <!-- Lyric content container -->
   <v-container fluid>
-    <v-row class="fill-height" align="center" justify="center">
-      <v-col class="fill-height" align="center" justify="center" cols="2">
-        <!-- Show search box -->
-        <div v-if="showInputField">
-          <v-text-field v-model="userInput" align="center" autofocus type="number"> </v-text-field>
-        </div>
-      </v-col>
-    </v-row>
     <v-row align="center">
       <v-col align="center">
         <!-- Lyric title and content -->
@@ -26,10 +34,10 @@
     </v-col>
   </v-row>
   </v-container>
+</div>
 </template>
 
 <script>
-import io from 'socket.io-client'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -57,38 +65,6 @@ export default {
     ...mapGetters({
       lyric: 'lyric/getCurrentLyric'
     })
-  },
-  methods: {
-    doCommand (e) {
-      const cmd = String.fromCharCode(e.keyCode).toLowerCase()
-      if (!isNaN(cmd)) {
-        clearTimeout(this.gotoTimeout)
-        this.showInputField = true
-        this.gotoTimeout = setTimeout(() => {
-          this.showInputField = false
-          if (this.userInput !== '') {
-            this.$router.push({ name: 'Project', params: { id: this.userInput }, query: { type: 'lyric' } })
-          }
-          this.userInput = ''
-        }, 1000)
-      }
-    }
-  },
-  created () {
-    // Setup window listener
-    window.addEventListener('keypress', this.doCommand)
-    // Socket.io
-    this.socket = io('http://' + window.location.hostname + ':3000')
-    //  Listen to Socket.io messages
-    this.socket.on('onIndexnType', data => {
-      this.$router.push({ name: 'Project', params: { id: data.index }, query: { type: data.type } })
-      // Temporary fix
-      location.reload()
-    })
-  },
-  destroyed () {
-    // Kill window listener in current component
-    window.removeEventListener('keypress', this.doCommand)
   },
   mounted () {
     this.$store.dispatch('lyric/fetchLyric', this.id)
