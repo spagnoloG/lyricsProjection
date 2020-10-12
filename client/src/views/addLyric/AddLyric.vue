8067ab3541e76e183e4ced205b3cf320ec27eefb
Gasper <gasper.spagnolo@outlook.com>
2020-10-12 15:48:37 +0200

    Refactored AddLyric component.

--- client/src/views/addLyric/AddLyric.vue
+++ client/src/views/addLyric/AddLyric.vue
@@ -1,174 +1,203 @@
 <template>
   <v-container fluid>
     <v-stepper v-model="e1">
-    <v-stepper-header>
-      <v-stepper-step :complete="e1 > 1" step="1">Vnesi naslov</v-stepper-step>
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1"
+          >Vnesi naslov</v-stepper-step
+        >
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-stepper-step :complete="e1 > 2" step="2">Izberi kategorijo</v-stepper-step>
+        <v-stepper-step :complete="e1 > 2" step="2"
+          >Izberi kategorijo</v-stepper-step
+        >
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-stepper-step step="3">Vnesi besedilo</v-stepper-step>
-    </v-stepper-header>
+        <v-stepper-step step="3">Vnesi besedilo</v-stepper-step>
+      </v-stepper-header>
 
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <v-card class="mb-12" color="grey lighten-1" height="200px">
-          <v-form>
-            <v-container>
-              <v-row>
-                <v-col align="center">
-                  <h2 class="font-weight-regular black--text">Vnesi naslov pesmi</h2>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col></v-col>
-                <v-col cols="12" sm="6">
-                  <form v-on:submit.prevent>
-                    <v-text-field
-                      v-on:keyup.enter="nextOne"
-                      v-model="title"
-                      label="Naslov pesmi"
-                      class="black--text"
-                      light
-                      outlined
-                      required
-                      autofocus
-                    ></v-text-field>
-                  </form>
-                </v-col>
-                <v-col></v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card class="mb-12" color="grey lighten-1" height="200px">
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col align="center">
+                    <h2 class="font-weight-regular black--text">
+                      Vnesi naslov pesmi
+                    </h2>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col></v-col>
+                  <v-col cols="12" sm="6">
+                    <form v-on:submit.prevent>
+                      <v-text-field
+                        v-on:keyup.enter="nextOne"
+                        v-model="title"
+                        label="Naslov pesmi"
+                        class="black--text"
+                        light
+                        outlined
+                        required
+                        autofocus
+                      ></v-text-field>
+                    </form>
+                  </v-col>
+                  <v-col></v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card>
 
-        <v-container fluid>
-          <v-row>
-            <v-col>
-              <v-btn color="primary" @click="nextOne">Nadaljuj</v-btn>
-            </v-col>
-            <v-col align="end">
-              <v-btn
-              text
-              :to="{ name: 'Home'}">Domov</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-stepper-content>
-
-      <v-stepper-content step="2">
-        <v-card class="mb-12" color="grey lighten-1" height="200px">
-          <v-form v-on:submit.prevent>
-            <v-container>
-              <v-row>
-                <v-col align="center">
-                  <h2 class="font-weight-regular black--text">Izberi kategorijo</h2>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col></v-col>
-                <v-col cols="12" sm="6">
-                  <v-select
-                    v-on:keyup.enter="e1++"
-                    v-model="selected"
-                    :items="categories"
-                    :rules="[v => !!v || 'Kategorija mora biti izbrana!']"
-                    label="Kategorija"
-                    light
-                    outlined
-                    multiple
-                    required
-                  ></v-select>
-                </v-col>
-                <v-col></v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card>
-
-        <v-container fluid>
-          <v-row>
-            <v-col>
-              <v-btn color="primary" @click="nextTwo">Nadaljuj</v-btn>
-              <v-btn text @click="--e1">Nazaj</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-stepper-content>
-
-      <v-stepper-content step="3">
-        <v-card class="mb-12" color="grey lighten-1">
-          <v-container>
+          <v-container fluid>
             <v-row>
-              <v-col align="center">
-                <h2 class="font-weight-regular black--text">Vnesi besedilo pesmi</h2>
+              <v-col>
+                <v-btn color="primary" @click="nextOne">Nadaljuj</v-btn>
               </v-col>
-            </v-row>
-            <v-row>
-              <v-col></v-col>
-              <v-col  cols="12" lg="6" align="center" justify="center">
-                <!-- Menu -->
-                <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-                  <div class="menubar">
-                    <v-btn
-                    :class="{ 'is-active': isActive.bold() }"
-                    @click="commands.bold"
-                    color="#950740"
-                    small
-                    dark
-                    fab
-                    >
-                    <v-icon>mdi-format-bold</v-icon>
-                    </v-btn>
-
-                    <v-btn
-                    :class="{ 'is-active': isActive.italic() }"
-                    @click="commands.italic"
-                    color="#950740"
-                    class="ma-1 white--text"
-                    fab
-                    small
-                    dark
-                    >
-                    <v-icon>mdi-format-italic</v-icon>
-                    </v-btn>
-                  </div>
-                </editor-menu-bar>
-                <br>
-                <!-- Editor -->
-                <editor-content class="editor-content" :editor="editor" focus />
+              <v-col align="end">
+                <v-btn text :to="{ name: 'Home' }">Domov</v-btn>
               </v-col>
-              <v-col></v-col>
             </v-row>
           </v-container>
-        </v-card>
+        </v-stepper-content>
 
-        <v-container fluid>
-          <v-row>
-            <v-col>
-              <v-btn text @click="--e1">Nazaj</v-btn>
-            </v-col>
-            <v-col align="end">
-              <v-btn color="primary" text @click="submitEntry">Shrani</v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper>
+        <v-stepper-content step="2">
+          <v-card class="mb-12" color="grey lighten-1" height="200px">
+            <v-form v-on:submit.prevent>
+              <v-container>
+                <v-row>
+                  <v-col align="center">
+                    <h2 class="font-weight-regular black--text">
+                      Izberi kategorijo
+                    </h2>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col></v-col>
+                  <v-col cols="12" sm="6">
+                    <v-select
+                      v-on:keyup.enter="e1++"
+                      v-model="selected"
+                      :items="categories"
+                      :rules="[v => !!v || 'Kategorija mora biti izbrana!']"
+                      label="Kategorija"
+                      light
+                      outlined
+                      multiple
+                      required
+                    ></v-select>
+                  </v-col>
+                  <v-col></v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card>
+
+          <v-container fluid>
+            <v-row>
+              <v-col>
+                <v-btn color="primary" @click="nextTwo">Nadaljuj</v-btn>
+                <v-btn text @click="--e1">Nazaj</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-stepper-content>
+
+        <v-stepper-content step="3">
+          <v-card class="mb-12" color="grey lighten-1">
+            <v-container>
+              <v-row>
+                <v-col align="center">
+                  <h2 class="font-weight-regular black--text">
+                    Vnesi besedilo pesmi
+                  </h2>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col></v-col>
+                <v-col cols="12" lg="6" align="center" justify="center">
+                  <!-- Menu -->
+                  <editor-menu-bar :editor="editor" v-slot="{ commands }">
+                    <div class="menubar">
+                      <v-item-group class="v-btn-toggle">
+                        <v-btn
+                          @click="commands.redo"
+                          color="primary"
+                          class="inactive"
+                          inactive
+                        >
+                          <v-icon>mdi-redo</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          class="inactive"
+                          @click="commands.bold"
+                          color="primary"
+                        >
+                          <v-icon>mdi-format-bold</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          class="inactive"
+                          @click="commands.italic"
+                          color="primary"
+                        >
+                          <v-icon>mdi-format-italic</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          class="inactive"
+                          @click="commands.undo"
+                          color="primary"
+                        >
+                          <v-icon>mdi-undo</v-icon>
+                        </v-btn>
+                      </v-item-group>
+                    </div>
+                  </editor-menu-bar>
+                  <br />
+                  <!-- Editor -->
+                  <editor-content
+                    class="editor-content"
+                    :editor="editor"
+                    focus
+                  />
+                </v-col>
+                <v-col></v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+
+          <v-container fluid>
+            <v-row class="fill-height">
+              <v-col align="start" justify="start">
+                <v-btn text @click="--e1">Nazaj</v-btn>
+              </v-col>
+              <v-col align="end" justify="end">
+                <v-btn color="primary" text @click="submitEntry">Shrani</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </v-container>
 </template>
 
 <script>
-import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+  Editor,
+  EditorContent,
+  EditorMenuBar
+} from 'tiptap'
 import { mapGetters } from 'vuex'
 
 import {
   Bold,
-  Italic
+  Italic,
+  History
 } from 'tiptap-extensions'
 
 export default {
@@ -182,7 +211,8 @@ export default {
       editor: new Editor({
         extensions: [
           new Bold(),
-          new Italic()
+          new Italic(),
+          new History()
         ],
         content: `
           <p>
@@ -201,7 +231,6 @@ export default {
   },
   computed: {
     ...mapGetters({
-      index: 'lyric/getNewLyricIndex',
       categories: 'lyric/getCategories'
     })
   },
@@ -222,7 +251,6 @@ export default {
       }
       // Form a document
       const document = {
-        index: this.index,
         title: this.title,
         categories: this.selected,
         content: this.content
@@ -232,13 +260,19 @@ export default {
       this.$router.push({ name: 'Home' })
     }
   },
-  created () {
-    this.$store.dispatch('lyric/fetchLyrics')
-    this.$store.dispatch('lyric/fetchCategories')
+  async created () {
+    await this.$store.dispatch('lyric/fetchLyrics')
+    await this.$store.dispatch('lyric/fetchCategories')
   },
-  beforeDestroy () {
-    this.$store.dispatch('lyric/fetchLyrics')
-    this.$store.dispatch('lyric/fetchCategories')
+  async beforeDestroy () {
+    await this.$store.dispatch('lyric/fetchLyrics')
+    await this.$store.dispatch('lyric/fetchCategories')
   }
 }
 </script>
+
+<style>
+.v-btn--active.inactive::before {
+  opacity: 0 !important;
+}
+</style>
