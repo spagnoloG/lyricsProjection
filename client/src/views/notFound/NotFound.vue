e642c9a100bea01e693caba2cf4ab4974ff93aca
Gasper <gasper.spagnolo@outlook.com>
2020-10-12 16:25:37 +0200

    updated 404 route

--- client/src/views/notFound/NotFound.vue
+++ client/src/views/notFound/NotFound.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <section class="error-container">
-      <span class="four"><span class="screen-reader-text">4</span></span>
-      <span class="zero"><span class="screen-reader-text">0</span></span>
-      <span class="four"><span class="screen-reader-text">4</span></span>
-   </section>
-   <v-container fluid>
-     <v-row class="fill-height">
-       <v-col align="center" justify="center">
-         <p class="font-weight-light">Te strani ni mogoče najti</p>
-       </v-col>
-     </v-row>
-     <v-row class="fill-height">
-       <v-col align="center" justify="center">
-         <v-btn text :to="{ name: 'Home' }" color="primary">Domov</v-btn>
-       </v-col>
-     </v-row>
-   </v-container>
+<section class="error-container">
+  <span>4</span>
+  <span><span class="screen-reader-text">0</span></span>
+  <span>4</span>
+</section>
+    <v-container fluid>
+      <v-row class="fill-height">
+        <v-col align="center" justify="center">
+          <p class="font-weight-light">Te strani ni mogoče najti</p>
+        </v-col>
+      </v-row>
+      <v-row class="fill-height">
+        <v-col align="center" justify="center">
+          <v-btn text :to="{ name: 'Home' }" color="primary">Domov</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script>
@@ -27,121 +27,146 @@ export default {
 <style>
 .error-container {
   text-align: center;
-  font-size: 106px;
-  font-family: 'Catamaran', sans-serif;
+  font-size: 180px;
+  font-family: "Catamaran", sans-serif;
   font-weight: 800;
-  margin: 70px 15px;
+  margin: 20px 15px;
+}
+.error-container > span {
+  display: inline-block;
+  line-height: 0.7;
+  position: relative;
+  color: #ffb485;
 }
 .error-container > span {
   display: inline-block;
   position: relative;
+  vertical-align: middle;
 }
-.error-container > span.four {
-  width: 136px;
-  height: 43px;
+.error-container > span:nth-of-type(1) {
+  color: #d1f2a5;
+  animation: colordancing 4s infinite;
+}
+.error-container > span:nth-of-type(3) {
+  color: #f56991;
+  animation: colordancing2 4s infinite;
+}
+.error-container > span:nth-of-type(2) {
+  width: 120px;
+  height: 120px;
   border-radius: 999px;
-  background:
-    linear-gradient(140deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.07) 43%, transparent 44%, transparent 100%),
-    linear-gradient(105deg, transparent 0%, transparent 40%, rgba(0, 0, 0, 0.06) 41%, rgba(0, 0, 0, 0.07) 76%, transparent 77%, transparent 100%),
-    linear-gradient(to right, #d89ca4, #e27b7e);
 }
-.error-container > span.four:before,
-.error-container > span.four:after {
-  content: '';
-  display: block;
+.error-container > span:nth-of-type(2):before,
+.error-container > span:nth-of-type(2):after {
+  border-radius: 0%;
+  content: "";
   position: absolute;
+  top: 0;
+  left: 0;
+  width: inherit;
+  height: inherit;
   border-radius: 999px;
+  box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
+    inset 0 30px 0 rgba(239, 250, 180, 0.4),
+    inset -30px 0 0 rgba(255, 196, 140, 0.4),
+    inset 0 -30px 0 rgba(245, 105, 145, 0.4);
+  animation: shadowsdancing 4s infinite;
 }
-.error-container > span.four:before {
-  width: 43px;
-  height: 156px;
-  left: 60px;
-  bottom: -43px;
-  background:
-    linear-gradient(128deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.07) 40%, transparent 41%, transparent 100%),
-    linear-gradient(116deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.07) 50%, transparent 51%, transparent 100%),
-    linear-gradient(to top, #99749D, #B895AB, #CC9AA6, #D7969E, #E0787F);
-}
-.error-container > span.four:after {
-  width: 137px;
-  height: 43px;
-  transform: rotate(-49.5deg);
-  left: -18px;
-  bottom: 36px;
-  background: linear-gradient(to right, #99749D, #B895AB, #CC9AA6, #D7969E, #E0787F);
-}
-
-.error-container > span.zero {
-  vertical-align: text-top;
-  width: 156px;
-  height: 156px;
-  border-radius: 999px;
-  background: linear-gradient(-45deg, transparent 0%, rgba(0, 0, 0, 0.06) 50%,  transparent 51%, transparent 100%),
-    linear-gradient(to top right, #99749D, #99749D, #B895AB, #CC9AA6, #D7969E, #ED8687, #ED8687);
-  overflow: hidden;
-  animation: bgshadow 5s infinite;
-}
-.error-container > span.zero:before {
-  content: '';
-  display: block;
-  position: absolute;
+.error-container > span:nth-of-type(2):before {
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
   transform: rotate(45deg);
-  width: 90px;
-  height: 90px;
-  background-color: transparent;
-  left: 0px;
-  bottom: 0px;
-  background:
-    linear-gradient(95deg, transparent 0%, transparent 8%, rgba(0, 0, 0, 0.07) 9%, transparent 50%, transparent 100%),
-    linear-gradient(85deg, transparent 0%, transparent 19%, rgba(0, 0, 0, 0.05) 20%, rgba(0, 0, 0, 0.07) 91%, transparent 92%, transparent 100%);
-}
-.error-container > span.zero:after {
-  content: '';
-  display: block;
-  position: absolute;
-  border-radius: 999px;
-  width: 70px;
-  height: 70px;
-  left: 43px;
-  bottom: 43px;
-  background: #ffffff;
-  box-shadow: -2px 2px 2px 0px rgba(0, 0, 0, 0.1);
 }
 
 .screen-reader-text {
-    position: absolute;
-    top: -9999em;
-    left: -9999em;
+  position: absolute;
+  top: -9999em;
+  left: -9999em;
 }
-
-@keyframes bgshadow {
+@keyframes shadowsdancing {
   0% {
-    box-shadow: inset -160px 160px 0px 5px rgba(0, 0, 0, 0.4);
+    box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
+      inset 0 30px 0 rgba(239, 250, 180, 0.4),
+      inset -30px 0 0 rgba(255, 196, 140, 0.4),
+      inset 0 -30px 0 rgba(245, 105, 145, 0.4);
   }
-  45% {
-    box-shadow: inset 0px 0px 0px 0px rgba(0, 0, 0, 0.1);
+  25% {
+    box-shadow: inset 30px 0 0 rgba(245, 105, 145, 0.4),
+      inset 0 30px 0 rgba(209, 242, 165, 0.4),
+      inset -30px 0 0 rgba(239, 250, 180, 0.4),
+      inset 0 -30px 0 rgba(255, 196, 140, 0.4);
   }
-  55% {
-    box-shadow: inset 0px 0px 0px 0px rgba(0, 0, 0, 0.1);
+  50% {
+    box-shadow: inset 30px 0 0 rgba(255, 196, 140, 0.4),
+      inset 0 30px 0 rgba(245, 105, 145, 0.4),
+      inset -30px 0 0 rgba(209, 242, 165, 0.4),
+      inset 0 -30px 0 rgba(239, 250, 180, 0.4);
+  }
+  75% {
+    box-shadow: inset 30px 0 0 rgba(239, 250, 180, 0.4),
+      inset 0 30px 0 rgba(255, 196, 140, 0.4),
+      inset -30px 0 0 rgba(245, 105, 145, 0.4),
+      inset 0 -30px 0 rgba(209, 242, 165, 0.4);
   }
   100% {
-    box-shadow: inset 160px -160px 0px 5px rgba(0, 0, 0, 0.4);
+    box-shadow: inset 30px 0 0 rgba(209, 242, 165, 0.4),
+      inset 0 30px 0 rgba(239, 250, 180, 0.4),
+      inset -30px 0 0 rgba(255, 196, 140, 0.4),
+      inset 0 -30px 0 rgba(245, 105, 145, 0.4);
+  }
+}
+@keyframes colordancing {
+  0% {
+    color: #d1f2a5;
+  }
+  25% {
+    color: #f56991;
+  }
+  50% {
+    color: #ffc48c;
+  }
+  75% {
+    color: #effab4;
+  }
+  100% {
+    color: #d1f2a5;
+  }
+}
+@keyframes colordancing2 {
+  0% {
+    color: #ffc48c;
+  }
+  25% {
+    color: #effab4;
+  }
+  50% {
+    color: #d1f2a5;
+  }
+  75% {
+    color: #f56991;
+  }
+  100% {
+    color: #ffc48c;
   }
 }
 
 /* demo stuff */
 * {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
 }
 body {
-  background-color: #FDFAF5;
+  background-color: #416475;
   margin-bottom: 50px;
 }
-html, button, input, select, textarea {
-    font-family: 'Montserrat', Helvetica, sans-serif;
-    color: #bbb;
+html,
+button,
+input,
+select,
+textarea {
+  font-family: "Montserrat", Helvetica, sans-serif;
+  color: #92a4ad;
 }
 h1 {
   text-align: center;
@@ -152,5 +177,23 @@ h1 {
   margin: 30px auto 30px;
   font-size: 19px;
   text-align: center;
+}
+.link-container {
+  text-align: center;
+}
+a.more-link {
+  text-transform: uppercase;
+  font-size: 13px;
+  background-color: #92a4ad;
+  padding: 10px 15px;
+  border-radius: 0;
+  color: #416475;
+  display: inline-block;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  line-height: 1.5;
+  text-decoration: none;
+  margin-top: 50px;
+  letter-spacing: 1px;
 }
 </style>
