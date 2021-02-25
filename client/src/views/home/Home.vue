b8d5afb7b4b04bcfae3ace356db363512d49159c
spagnolog <gasper.spagnolo@outlook.com>
2021-02-25 20:31:11 +0100

    Uploading local commits

--- client/src/views/home/Home.vue
+++ client/src/views/home/Home.vue
@@ -3,30 +3,45 @@
     <v-row>
       <v-col>
         <v-img
-        :lazy-src="require(`@/assets/homePage-lazy.jpg`)"
-        :src="require(`@/assets/homePage.jpg`)"
-        max-height="600" contain>
-       </v-img>
+          :lazy-src="require(`@/assets/homePage-lazy.jpg`)"
+          :src="require(`@/assets/homePage.jpg`)"
+          max-height="600"
+          contain
+        >
+        </v-img>
       </v-col>
     </v-row>
     <v-row>
-      <v-col align="center" cols=12 sm=6>
-        <v-btn
-        :to="{ name: 'ListLyrics'}"
-        color="primary">{{ $t('List Of Lyrics') }}<v-icon right>mdi-playlist-music</v-icon></v-btn>
+      <v-col align="center" cols="12" sm="6">
+        <v-btn :to="{ name: 'ListLyrics' }" color="primary"
+          >{{ $t("List Of Lyrics")
+          }}<v-icon right>mdi-playlist-music</v-icon></v-btn
+        >
       </v-col>
-      <v-col align="center" cols=12 sm=6>
-        <v-btn
-        :to="{name: 'AddLyric'}"
-        color="primary">Add new Lyric<v-icon right>mdi-music</v-icon></v-btn>
+      <v-col align="center" cols="12" sm="6">
+        <v-btn :to="{ name: 'AddLyric' }" color="primary"
+          >Add new Lyric<v-icon right>mdi-music</v-icon></v-btn
+        >
       </v-col>
     </v-row>
     <v-row>
       <v-col align="center">
-        <v-btn
-        :to="{name: 'Remote'}"
-        color="#59A96A"
-        large>Remote<v-icon right>mdi-remote</v-icon></v-btn>
+        <v-btn :to="{ name: 'Remote' }" color="#59A96A" large
+          >Remote<v-icon right>mdi-remote</v-icon></v-btn
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <ul>
+          <li
+            v-for="locale in locales"
+            :key="locale"
+            @click="switchLocale(locale)"
+          >
+            {{ locale }}
+          </li>
+        </ul>
       </v-col>
     </v-row>
   </v-container>
@@ -42,6 +57,9 @@ export default {
 {
   "en": {
     "List Of Lyrics": "List Of Lyrics"
+  },
+  "si": {
+    "List Of Lyrics": "Seznam Pesmi"
   }
 }
 </i18n>
