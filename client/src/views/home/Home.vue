f4ab9e7ea1ae10447771603f6e43d17cb4c2c9f8
spagnolog <gasper.spagnolo@outlook.com>
2021-02-26 01:47:13 +0100

    Localized home page

--- client/src/views/home/Home.vue
+++ client/src/views/home/Home.vue
@@ -20,28 +20,17 @@
       </v-col>
       <v-col align="center" cols="12" sm="6">
         <v-btn :to="{ name: 'AddLyric' }" color="primary"
-          >Add new Lyric<v-icon right>mdi-music</v-icon></v-btn
+          >{{ $t("Add new Lyric")
+          }}<v-icon right>mdi-music</v-icon></v-btn
         >
       </v-col>
     </v-row>
     <v-row>
       <v-col align="center">
         <v-btn :to="{ name: 'Remote' }" color="#59A96A" large
-          >Remote<v-icon right>mdi-remote</v-icon></v-btn
+          >{{ $t("Remote")
+          }}<v-icon right>mdi-remote</v-icon></v-btn
         >
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <ul>
-          <li
-            v-for="locale in locales"
-            :key="locale"
-            @click="switchLocale(locale)"
-          >
-            {{ locale }}
-          </li>
-        </ul>
       </v-col>
     </v-row>
   </v-container>
@@ -56,10 +45,14 @@ export default {
 <i18n>
 {
   "en": {
-    "List Of Lyrics": "List Of Lyrics"
+    "List Of Lyrics" : "List Of Lyrics",
+    "Remote" : "Remote",
+    "Add new Lyric" : "Add new Lyric"
   },
   "si": {
-    "List Of Lyrics": "Seznam Pesmi"
+    "List Of Lyrics": "Seznam Pesmi",
+    "Remote" : "Nazdiraj",
+    "Add new Lyric" : "Dodaj novo pesem"
   }
 }
 </i18n>
